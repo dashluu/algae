32abc0ac534a2442d5eecccee5b4b6cc10dd4a75
dashluu <dashluu9121997@gmail.com>
2024-06-02 23:49:50 -0700

    Rewrote URLs.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 import ErrorPage from "./ErrorPage.jsx";
-import MaterialGrid, {loader as materialLoader} from "./material/MaterialGrid.jsx";
 import AlgaeInfoPage from "./algae/AlgaeInfoPage.jsx";
 import KitPage from "./kit/KitPage.jsx";
 import AboutPage from "./about/AboutPage.jsx";
